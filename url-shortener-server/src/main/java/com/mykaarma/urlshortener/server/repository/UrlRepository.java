a6fd4044e70f41d925df3d1aeb7c1fee645250bd
EC2AMAZ-UGCR49F\Admin <karan.chugh@mykaarma.com>
2021-07-13 03:01:09 +0000

    Removed Parent Pom Dependency

--- url-shortener-server/src/main/java/com/mykaarma/urlshortener/server/repository/UrlRepository.java
+++ url-shortener-server/src/main/java/com/mykaarma/urlshortener/server/repository/UrlRepository.java
@@ -1,9 +1,6 @@
 package com.mykaarma.urlshortener.server.repository;
 
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,14 +14,14 @@ import java.util.*;
 public interface UrlRepository extends MongoRepository<UrlAttributes,Long> {
 	
 		
-		@CacheEvict(value="URL",key="#id")
+
 		public void deleteBySecondaryId(long id);
 		
 		
 		public List<UrlAttributes> findByLongUrl(String longUrl);
 	
 		
-		@Cacheable("URL")
+
 		public List<UrlAttributes> findBySecondaryId(long id);
 		
 		
