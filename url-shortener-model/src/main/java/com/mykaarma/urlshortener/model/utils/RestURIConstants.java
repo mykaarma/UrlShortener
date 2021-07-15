b2f5d0c4578920043417ff4b06967635bc6e0bb1
EC2AMAZ-UGCR49F\Admin <karan.chugh@mykaarma.com>
2021-07-15 07:27:56 +0000

    Added Context path

--- url-shortener-model/src/main/java/com/mykaarma/urlshortener/model/utils/RestURIConstants.java
+++ url-shortener-model/src/main/java/com/mykaarma/urlshortener/model/utils/RestURIConstants.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 
 public class RestURIConstants {
 
+	public static final String URL_SHORTENER ="urlshortener";
 	public static final String V2 = "/v2";
 	public static final String SHORTEN = "shorten";
 	public static final String REDIRECT = "redirect";
@@ -14,6 +15,7 @@ public class RestURIConstants {
 	public static final String SHORT_URL_DOMAIN_PATH_VARIABLE = "{shortUrlDomain}";
 	public static final String SHORT_URL_HASH_PATH_VARIABLE = "{shortUrlHash}";
 	public static final String COUNT_CLICKS = "countClicks";
+	
 	
 
 	
