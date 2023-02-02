81396d2b6cb6d53b63ef031679e4704fe031cebd
AadiS11 <aadi.shukla@mykaarma.com>
2023-02-02 22:28:21 +0530

    Added default constructor

--- src/main/java/com/mykaarma/urlshortener/model/UrlDetails.java
+++ src/main/java/com/mykaarma/urlshortener/model/UrlDetails.java
@@ -5,11 +5,15 @@ import java.util.Date;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class UrlDetails implements Serializable {
 	
