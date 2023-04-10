b7fc3280f2b97dcdde599ebe1ae31d5a312db955
harshit050499 <43980032+harshit050499@users.noreply.github.com>
2023-04-10 17:00:27 +0530

    Update UrlService.java

--- src/main/java/com/mykaarma/urlshortener/service/UrlService.java
+++ src/main/java/com/mykaarma/urlshortener/service/UrlService.java
@@ -131,9 +131,7 @@ public class UrlService {
 		if(urlPrefix != null && !urlPrefix.isEmpty() && urlPrefix.charAt(urlPrefix.length()-1)!='/') {
 			urlPrefix += '/';
 		}
-		else {
-			urlPrefix = "";
-		}
+
 		String shortUrl = shortUrlDomain + urlPrefix + shortUrlHash;
 		
 		shortUrlDetails.setShortUrl(shortUrl);
